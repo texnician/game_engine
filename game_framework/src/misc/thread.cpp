882148b82d5767d06ee8db3c8c6f8ac30ee2bf6a
Tang Yaguang <texnician@gmail.com>
2011-06-06 12:49:01 +0000

    atomic pod

--- game_framework/src/misc/thread.cpp
+++ game_framework/src/misc/thread.cpp
@@ -1,8 +1,15 @@
 #include <pthread.h>
 #include<semaphore.h>
 #include "../define.h"
+#include <cstdatomic>
+#include "../atomic_pod.h"
 
-volatile int cnt = 0;
+// volatile int cnt = 0;
+
+// volatile std::atomic<int> cnt(0);
+
+volatile atomic_pod<int> cnt(0);
+
 sem_t mutex;
 
 void *thread(void *argp);
@@ -10,9 +17,16 @@ void *thread(void *argp);
 int main(int argc, char *argv[])
 {
 
-    int niters = 10000;
+    int niters = 100000000;
 
     sem_init(&mutex, 0, 1);
+
+    
+    printf("%d\n", _GLIBCXX_ATOMIC_BUILTINS_1);
+    printf("%d\n", _GLIBCXX_ATOMIC_BUILTINS_2);
+    printf("%d\n", _GLIBCXX_ATOMIC_BUILTINS_4);
+    printf("%d\n", _GLIBCXX_ATOMIC_BUILTINS_8);
+    
     
     pthread_t tid1, tid2;
     if (pthread_create(&tid1, 0, thread, &niters) < 0) {
@@ -25,10 +39,10 @@ int main(int argc, char *argv[])
     pthread_join(tid2, 0);
 
     if (cnt != (2 * niters)) {
-        printf("BOOM! cnt = %d\n", cnt);
+        printf("BOOM! cnt = %d\n", cnt.load());
     }
     else {
-        printf("OK! cnt = %d\n", cnt);
+        printf("OK! cnt = %d\n", cnt.load());
     }
     exit(0);
 }
@@ -37,9 +51,11 @@ void *thread(void *argp)
 {
     int niters = *((int *)argp);
     for (int i = 0; i < niters; i++) {
-        sem_wait(&mutex);
-        cnt++;
-        sem_post(&mutex);
+        // sem_wait(&mutex);
+        //cnt++;
+        ++cnt;
+        // __sync_fetch_and_add(&cnt, 1);
+        // sem_post(&mutex);
     }
         
     return 0;
