f71f11b96f3fa8417d8dbcf8f2eb793a45d34b68
Tang Yaguang <texnician@gmail.com>
2011-07-24 18:23:04 +0000

    add carray.py

---
+++ game_framework/src/carray.py
@@ -0,0 +1,19 @@
+class CArray:
+    def __init__(self, ptr, num):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def __len__(self):
+        pass
+    
+    def __contains__(self):
+        pass
+
+    def __missing__(self):
+        pass
+    
